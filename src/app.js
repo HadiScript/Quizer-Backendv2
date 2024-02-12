583976a8a8529efc63c2276d2da2867295106b90
HadiScript <2019cs519@gmail.com>
2024-02-12 18:16:24 +0500

    some

--- src/app.js
+++ src/app.js
@@ -24,10 +24,17 @@ app.use(
 app.use(
   cookieSession({
     signed: false,
-    // secure: process.env.NODE_ENV === "production",
     secure: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://quizer-frontend.vercel.app"); // Replace with the domain you want to allow
+  res.header("Access-Control-Allow-Credentials", "true");
+  // Include other headers as needed
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // APIS
 app.use("/api/auth", require("./routers/auth"));
