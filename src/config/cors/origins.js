16b00adacd885ae29ab3ed10f9279e16e8e38d26
HadiScript <2019cs519@gmail.com>
2024-02-12 17:15:16 +0500

    aaaa

--- src/config/cors/origins.js
+++ src/config/cors/origins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  "https://quizer-backendv2.vercel.app",
   "http://localhost:5173",
+  "https://quizer-backendv2.vercel.app",
   // "http://localhost:4173",
   // "http://localhost:8080",
   // "https://quizer.cycarts.com",
