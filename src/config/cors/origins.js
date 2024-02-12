9b5cb77167b1a78b007ca96f0c387b7bd9b678ec
HadiScript <2019cs519@gmail.com>
2024-02-12 15:40:34 +0500

    asd

--- src/config/cors/origins.js
+++ src/config/cors/origins.js
@@ -1,3 +1,9 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:4173", "http://localhost:8080", "https://quizer.cycarts.com"];
+const allowedOrigins = [
+  "https://quizer-backendv2.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:4173",
+  "http://localhost:8080",
+  "https://quizer.cycarts.com",
+];
 
 module.exports = allowedOrigins;
