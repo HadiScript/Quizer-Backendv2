1eb32d0bc592027970f13e3a6be5accaa3fe34f1
HadiScript <2019cs519@gmail.com>
2024-03-08 16:04:30 +0500

    clusterings

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-// const cluster = require("cluster");
+const cluster = require("cluster");
 
-// if (cluster.isMaster) {
-//   cluster.fork();
-//   cluster.fork();
-// } else {
-const { app } = require("./src/app");
-const connectDB = require("./src/config/datebase/db");
-const express = require("express");
+if (cluster.isMaster) {
+  cluster.fork();
+  cluster.fork();
+} else {
+  const { app } = require("./src/app");
+  const connectDB = require("./src/config/datebase/db");
+  const express = require("express");
 
+  require("dotenv").config();
 
+  const startUp = async () => {
+    await connectDB();
+    app.listen(process.env.PORT || 8080, () => {
+      console.log("Listening on port 8080!");
+    });
+  };
 
-require("dotenv").config();
-
-const startUp = async () => {
-  await connectDB();
-  app.listen(process.env.PORT || 8080, () => {
-    console.log("Listening on port 8080!");
-  });
-};
-
-startUp();
-// }
+  startUp();
+}
